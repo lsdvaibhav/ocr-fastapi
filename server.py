41372232e157343061a18e28c728c8c1150e59e1
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2021-01-27 14:21:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -17,7 +17,8 @@ def home(request: Request):
 async def extract_text(image: UploadFile = File(...)):
     temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
     text = await ocr.read_image(temp_file)
-    return {"filename": image.filename, "text": text}
+    data   = genrateData(text)
+    return {"filename": image.filename, "text": data}
 
 @app.post("/api/v1/bulk_extract_text")
 async def bulk_extract_text(request: Request, bg_task: BackgroundTasks):
@@ -37,6 +38,7 @@ async def bulk_output(task_id):
     for file_ in os.listdir(task_id):
         if file_.endswith("txt"):
             text_map[file_] = open(os.path.join(task_id, file_)).read()
+            text_map = genrateData(text_map)
     return {"task_id": task_id, "output": text_map}
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
@@ -45,3 +47,56 @@ def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
     with open(temp_file, "wb") as buffer:
         shutil.copyfileobj(uploaded_file.file, buffer)
     return temp_file
+
+def genrateData(text):
+    text = text.replace('[',' ')
+    text = text.replace(']',' ')
+    text= text.replace('|',' ')
+    text = re.sub(' +', ' ',text)
+    text = text.lower()
+    text = text[text.find('registration number :'):text.find('note')]
+    text1= ''
+    datalist = []
+    for each in text.split('\n'):
+        if len(each) >1:
+            datalist.append(each)
+            for eachh in each.split(' '):
+                if  eachh == '' or eachh== ' ':
+                    pass
+                else :
+                    text1 =  text1+ eachh+' '
+
+    rn = text1[text1.find('registration number :'):text1.find('legal name')]
+    ln = text1[text1.find('legal name'):text1.find('trade name')]
+    tnia = text1[text1.find('trade name, if any'):text1.find('constitution of business')]
+    cob = text1[text1.find('constitution of business'):text1.find('address of principal place of')]
+    addBusiness = text1[text1.find('address of principal place of'):]
+    address = addBusiness[addBusiness.find('address of principal place of'):addBusiness.find('business')]+addBusiness[addBusiness.find('business')+8:addBusiness.find('date of liability')]
+    pov = addBusiness[addBusiness.find('period of validity'):addBusiness.find('type of registration')]
+    tor = addBusiness[addBusiness.find('type of registration'):addBusiness.find('particulars of approving authority')]
+    poaa = addBusiness[addBusiness.find('particulars of approving authority'):addBusiness.find('signature')]
+    sign = addBusiness[addBusiness.find('signature'):addBusiness.find('name')]
+    name = addBusiness[addBusiness.find('name'):addBusiness.find('designation')]
+    des = addBusiness[addBusiness.find('designation'):addBusiness.find('jurisdictional office')]
+    jo = addBusiness[addBusiness.find('jurisdictional office'):addBusiness.find('date of issue of certificate')]
+    doi = addBusiness[addBusiness.find('date of issue of certificate'):addBusiness.find('note')]
+    
+    data = {
+        "registrationNumber":rn,
+        "legalName":ln,
+        "tradeName":tnia,
+        "cob":cob,
+        "addres":address,
+        "dol":dol,
+        "pov":pov,
+        "tor":tor,
+        "poaa":poaa,
+        "sign":sign,
+        "name":name,
+        "des":des,
+        "jo":jo,
+        "doi":doi,
+        "datalist":datalist
+
+    }
+    return data 
